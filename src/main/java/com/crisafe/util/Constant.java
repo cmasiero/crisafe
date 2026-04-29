b87ab67f1e30f3f4528fe72515a790ec2f65ffe1
C.Masiero <c.masiero@be-tse.it>
2026-04-29 11:12:01 +0200

    sonarcube

--- src/main/java/com/crisafe/util/Constant.java
+++ src/main/java/com/crisafe/util/Constant.java
@@ -2,5 +2,10 @@ package com.crisafe.util;
 
 public class Constant {
 
+    private Constant() {
+        /* This utility class should not be instantiated */
+    }
+
     public static final String FIELD_SEP = "\u001F";
+
 }
